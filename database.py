3f203e7823b85b72cd18e17107fe5e95775887ed
Przemyslaw Tutur <przemyslaw.tutur@dell.com>
2024-02-25 14:06:03 +0100

    fix: Correction database module

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from databases import Database
 
 DATABASE_URL = "sqlite:///./test.db"
-# PostgreSQL GCP: DATABASE_URL = "postgresql://user:password@localhost/dbname"
+# PostgreSQL "postgresql://user:password@localhost/dbname"
 
-engine = create_engine(DATABASE_URL)
-metadata = MetaData()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Asynchroniczna baza danych
 database = Database(DATABASE_URL)
