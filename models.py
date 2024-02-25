aeb9934adea6334af85b6bb0da2276aa62dabcd5
Przemyslaw Tutur <przemyslaw.tutur@dell.com>
2024-02-25 13:47:29 +0100

    feat: Update models to handle db

--- models.py
+++ models.py
@@ -1,27 +1,80 @@
 from pydantic import BaseModel
 from typing import List, Optional
+from datetime import date
 
-class Experience(BaseModel):
+
+class ExperienceBase(BaseModel):
     company: str
     position: str
-    from_date: str
-    to_date: Optional[str] = None
+    from_date: date
+    to_date: Optional[date] = None
     description: Optional[str] = None
 
-class Education(BaseModel):
+
+class ExperienceCreate(ExperienceBase):
+    pass
+
+
+class Experience(ExperienceBase):
+    id: int
+    cv_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class EducationBase(BaseModel):
     institution: str
     degree: str
-    from_date: str
-    to_date: Optional[str] = None
+    from_date: date
+    to_date: Optional[date] = None
 
-class Skill(BaseModel):
+
+class EducationCreate(EducationBase):
+    pass
+
+
+class Education(EducationBase):
+    id: int
+    cv_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SkillBase(BaseModel):
     name: str
-    level: str # Może być 'podstawowy', 'średniozaawansowany', 'zaawansowany'
+    level: str
 
-class CV(BaseModel):
+
+class SkillCreate(SkillBase):
+    pass
+
+
+class Skill(SkillBase):
+    id: int
+    cv_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class CVBase(BaseModel):
     name: str
     email: str
-    experience: List[Experience]
-    education: List[Education]
-    skills: List[Skill]
 
+
+class CVCreate(CVBase):
+    experience: List[ExperienceCreate] = []
+    education: List[EducationCreate] = []
+    skills: List[SkillCreate] = []
+
+
+class CV(CVBase):
+    id: int
+    experience: List[Experience] = []
+    education: List[Education] = []
+    skills: List[Skill] = []
+
+    class Config:
+        orm_mode = True
